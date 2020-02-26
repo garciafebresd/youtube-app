a2abeb4e420c3641162a62f4a78e00e79d7b51c4
daniel garcia <dgarcia@position.cl>
2020-02-26 17:00:34 -0300

    change name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('firebase-fileupload-app app is running!');
+    expect(page.getTitleText()).toEqual('youtube-app app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firebase-fileupload-app'`, () => {
+  it(`should have as title 'youtube-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('firebase-fileupload-app');
+    expect(app.title).toEqual('youtube-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('firebase-fileupload-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('youtube-app app is running!');
   });
 });
